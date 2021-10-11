d501c0b5f2aa7abe1f5af7d421d5c8f97d2539cd
Mike <pegas19881@gmail.com>
2021-10-12 00:19:34 +0300

    new teamplates

--- src/main/java/com/bitrix24/webhook/controller/MainController.java
+++ src/main/java/com/bitrix24/webhook/controller/MainController.java
@@ -4,13 +4,16 @@ import com.bitrix24.webhook.service.MainServiceImpl;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+import java.util.HashMap;
+
+@Controller
 @RequiredArgsConstructor
 @RequestMapping("/")
 public class MainController {
@@ -18,56 +21,54 @@ public class MainController {
     @Autowired
     final MainServiceImpl mainService;
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'BOSS')")
     @GetMapping(value = "/main")
-    public ModelAndView findAll() {
-        return new ModelAndView("main");
-    }
-
-    @PreAuthorize("hasAnyRole('ADMIN', 'BOSS')")
-    @GetMapping(value = "/statements")
-    public ModelAndView statementsGet(Model model, MainServiceImpl mainService) throws JsonProcessingException {
-        model.addAttribute("entity", mainService.deserialization(mainService.findOne(getId())));
-        return new ModelAndView("statements");
+    public String findAll() {
+        return "main";
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'BOSS')")
     @GetMapping(value = "/list-of-statements")
-    public ModelAndView listOfStatements(Model model, MainServiceImpl mainService) throws JsonProcessingException {
-        model.addAttribute("list", mainService.deserialization(mainService.findAll()));
-        return new ModelAndView("listOfStatements");
+    public String listOfStatements(HashMap<String, Object> model, MainServiceImpl mainService) throws JsonProcessingException {
+        model.put("list", mainService.deserialization(mainService.findAll()));
+        return "statementsList";
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'BOSS')")
-    @PostMapping(value = "/list-of-statements", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ModelAndView listOfStatements(@RequestBody String id) {
+    @PostMapping(value = "/list-of-statements")
+    public String listOfStatements(@RequestBody String id) {
         String[] arrayId = id.split("=");
         setId(arrayId[1]);
-        return new ModelAndView("redirect:/statements");
+        return "redirect:/statement";
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'BOSS')")
     @GetMapping(value = "/order")
-    public ModelAndView orders() {
-        return new ModelAndView("order");
+    public String orders() {
+        return "order";
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'BOSS')")
-    @PostMapping(value = "/statements", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ModelAndView statements(@RequestBody String id, Model model, MainServiceImpl mainService) throws JsonProcessingException {
+    @PostMapping(value = "/statement")
+    public String statementsPost(@RequestBody String id, String comment, MainServiceImpl mainService) {
         String[] arrayId = id.split("=");
-        mainService.changeStatusToAcceptOrCancel(arrayId[1]);
-        model.addAttribute("list", mainService.deserialization(mainService.findAll()));
-        return new ModelAndView("redirect:/list-of-statements");
+        mainService.changeStatusToAcceptOrCancel(arrayId[2], comment);
+        return "redirect:/list-of-statements";
     }
 
-    private String resultId;
-
-    public void setId(String id) {
-        resultId = id;
+    @PreAuthorize("hasAnyRole('ADMIN', 'BOSS')")
+    @GetMapping(value = "/statement")
+    public String statements(HashMap<String, Object> model, MainServiceImpl mainService) throws JsonProcessingException {
+        model.put("user", mainService.deserialization(mainService.findOne(getId())));
+        return "statement";
     }
 
-    public String getId() {
-        return resultId;
+    private String id;
+
+    private void setId(String id) {
+        this.id = id;
+    }
+
+    private String getId() {
+        return id;
     }
 }
